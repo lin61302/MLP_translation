7e2ed42a6e1ba00b46e3f9686e96b1fb78e8a896
ZR <lin61302@gmail.com>
2025-04-04 23:37:45 -0400

    translation (dict_keys(['PAK', 'ALB', 'IND'])) update

--- translate.py
+++ translate.py
@@ -276,7 +276,7 @@ if __name__ == '__main__':
 
     # lan_dic = {'MOZ': ['pt2'], 'IDN': ['id'], 'CMR':['fr'], 'MAR':['ar','fr'], 'AGO': ['pt2'], 'PRY':['es'], 'MRT':['ar','fr']}
     # lan_dic = { 'DZA':['fr','ar'], 'ALB':['sq'],'MKD':['mk','sq'],'KHM':['km2'],'UKR':['uk','ru'],'UZB':['ru','uz'] }
-    lan_dic = {'PAK':['ur'],'ALB':['sq']}
+    lan_dic = {'PAK':['ur'],'ALB':['sq'],'IND':['hi']}
 
 
     for country, languages in lan_dic.items():
