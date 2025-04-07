6c3fb9c70c8c3f55f4bff8f9362a93610e94188a
ZR <lin61302@gmail.com>
2025-04-07 10:37:59 -0400

    translation (dict_keys(['AGO'])) update

--- translate.py
+++ translate.py
@@ -276,7 +276,7 @@ if __name__ == '__main__':
 
     # lan_dic = {'MOZ': ['pt2'], 'IDN': ['id'], 'CMR':['fr'], 'MAR':['ar','fr'], 'AGO': ['pt2'], 'PRY':['es'], 'MRT':['ar','fr']}
     # lan_dic = { 'DZA':['fr','ar'], 'ALB':['sq'],'MKD':['mk','sq'],'KHM':['km2'],'UKR':['uk','ru'],'UZB':['ru','uz'] }
-    lan_dic = {'MAR':['fr','ar'],'SSD':['ar'],'RWA':['rw','fr'],'BEN':['fr'],'COD':['fr'],'NER':['fr']}
+    lan_dic = {'NIC':['es']}
 
 
     for country, languages in lan_dic.items():
