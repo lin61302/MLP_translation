fa3aeb12ef101cfb5aec943a7e19110f82edfc7f
ZR <lin61302@gmail.com>
2025-04-27 16:34:34 -0400

    translation (dict_keys(['ENV_CMR', 'ENV_UZB', 'ENV_KHM'])) update

--- translate.py
+++ translate.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
     # lan_dic = {'MOZ': ['pt2'], 'IDN': ['id'], 'CMR':['fr'], 'MAR':['ar','fr'], 'AGO': ['pt2'], 'PRY':['es'], 'MRT':['ar','fr']}
     # lan_dic = { 'DZA':['fr','ar'], 'ALB':['sq'],'MKD':['mk','sq'],'KHM':['km2'],'UKR':['uk','ru'],'UZB':['ru','uz'] }
     # lan_dic = {'ENV_UZB':['uz']}
-    lan_dic = {'IDN':['id'],'PAN':['es'],'MKD':['sq','mk'],'KGZ':['kg','ru'],'MDA':['ro','ru'],'SEN':['fr']}#,'ENV_':[],'ENV_':[]}
+    lan_dic = {'ENV_CMR':['fr','ar'],'ENV_UZB':['uz'],'ENV_KHM':['km2']}#,'ENV_':[],'ENV_':[]}
 
 
     for country, languages in lan_dic.items():
